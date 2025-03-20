c11b9d284ae1a1480bb6d0ee05b2dd21fe5fb0de
Alex Valdelvira Muñoz <avm157@mscloud.ua.es>
2025-03-20 18:12:15 +0100

    Paso de antes de mongoDB

--- backEnd/controllers/assetController.js
+++ backEnd/controllers/assetController.js
@@ -1,26 +1,27 @@
 // Se encarga de manejar las peticiones de los endpoints de la API
 
-
+const asyncHandler = require("express-async-handler");
 const { get } = require("http");
 
-const getAsset = (req, res) =>{
+const getAsset = asyncHandler( async(req, res) =>{
     res.status(200).json({message: 'get all assets'});
-}
+});
 
-const postAsset = (req, res) =>{
-    if(!req.body.text){
-        res.status(400).json({message: 'El campo de texto del asset es requerido'});
+const postAsset = asyncHandler (async (req, res, next) => {
+    if (!req.body.text) {
+        return res.status(400).json({ message: "El campo de texto del asset es requerido" });
     }
-    res.status(200).json({message: 'create an asset'});
-}
+    res.status(200).json({ message: "Asset created Correctly" });
+});
 
-const putAsset = (req, res) =>{
+
+const putAsset = asyncHandler (async(req, res) =>{
     res.status(200).json({message: `Update asset with id: ${req.params.id}`});
-}
+});
 
-const deleteAsset = (req, res) =>{
+const deleteAsset = asyncHandler (async(req, res) =>{
     res.status(200).json({message: `Deleted asset with id: ${req.params.id}`});
-}
+});
 
 
 module.exports = {
@@ -28,6 +29,4 @@ module.exports = {
     postAsset,
     putAsset,
     deleteAsset
-
-
 }
--- backEnd/server.js
+++ backEnd/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv =  require('dotenv');
 const { urlencoded } = require('body-parser');
+const {errorHandler} = require('./middlewares/errorMiddlewares.js');
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.listen(port, () => console.log(`server is running on port: ${port}`));
 
+
+app.listen(port, () => console.log(`server is running on port: ${port}`));
+app.use(errorHandler);
 app.use('/api/assets', require('./routes/assetsRoutes'));
+
+
+//usar npm run server para correr el servidor
