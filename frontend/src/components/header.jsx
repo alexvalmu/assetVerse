b740d1b1610b7f639ce989640eaaedf4344bfb82
carolina-san <csa71@gcloud.ua.es>
2025-03-23 21:37:07 +0100

    minuto 37 video

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -4,22 +4,22 @@ import {Link} from 'react-router-dom'
 function header() {
   return (
     <header className='header'>
-        <div className='logo'>
-            <Link to='/'>Assets</Link>
-        </div>
-        <ul>
-            <li>
-                <Link to='/login'>
-                    <FaSignInAlt /> Login
-                </Link>
-            </li>
-            <li>
-                <Link to='/register'>
-                    <FaUser /> Register
-                </Link>
-            </li>
-        </ul>
-    </header>
+    <div className='logo'>
+        <Link to='/'>Assets</Link>
+    </div>
+    <ul>
+        <li>
+            <Link to='/login'>
+                <FaSignInAlt /> Login
+            </Link>
+        </li>
+        <li>
+            <Link to='/register'>
+                <FaUser /> Register
+            </Link>
+        </li>
+    </ul>
+</header>
   )
 }
 
