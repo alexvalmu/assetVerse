46c5c5e8fee0cfc8634c11f4883b8e4525a905c8
carolina-san <csa71@gcloud.ua.es>
2025-03-25 18:58:12 +0100

    funciona todo auth

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,13 +1,32 @@
 import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import {logout,reset} from '../features/auth/authSlice'
 
-function header() {
+function Header() {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const {user} = useSelector((state)=>state.auth);
+
+    const onLogout = ()=>{
+        dispatch(logout());
+        dispatch(reset());
+        navigate('/');
+    }
   return (
     <header className='header'>
     <div className='logo'>
         <Link to='/'>Assets</Link>
     </div>
     <ul>
+        {user ? (
+             <li>
+             <button className='btn' onClick={(onLogout)}>  
+                 <FaSignOutAlt /> Logout
+             </button>
+         </li>
+        ):(<>
         <li>
             <Link to='/login'>
                 <FaSignInAlt /> Login
@@ -18,9 +37,10 @@ function header() {
                 <FaUser /> Register
             </Link>
         </li>
+        </>)}
     </ul>
 </header>
   )
 }
 
-export default header
+export default Header
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,10 @@
 import {useState, useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import {toast} from 'react-toastify';
 import { FaUser } from 'react-icons/fa';
+import {register,reset} from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 function Register() {
     const[formData, setFormData] = useState({
@@ -9,6 +14,23 @@ function Register() {
         password2: ''
     })
     const {name, email, password, password2} = formData;
+
+    const navigate=useNavigate();
+    const dispatch = useDispatch();
+
+    const {user, isError, isSuccess, isLoading, message} = useSelector((state)=>state.auth);
+    
+    useEffect(()=>{
+        if(isError){
+            toast.error(message);
+        }
+        if(isSuccess||user){
+            navigate('/');
+        }
+
+        dispatch(reset());
+    },[user,isError,isSuccess,message,navigate,dispatch])
+
     const onChange = (e)=>{
         setFormData((prevState)=>({
             ...prevState,
@@ -17,9 +39,25 @@ function Register() {
     };
     const onSubmit = (e)=>{
         e.preventDefault();
+
+        if(password !== password2){
+            toast.error('Passwords do not match');
+        }
+        else{
+            const userData={
+                name,
+                email,
+                password
+            }
+            dispatch(register(userData));
+        }
     };
 
-  return (
+    if(isLoading){
+        return <Spinner />
+    }
+
+    return (
     <>
         <section className="heading">
             <h1><FaUser /> Register</h1>
