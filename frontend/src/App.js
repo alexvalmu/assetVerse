ab68c34f2b439ec06c5ce41bc74ecdd73f2a4ba7
paulaaortu <poj5@gcloud.ua.es>
2025-03-26 20:12:45 +0100

    solo falta arreglar menu hamburguesa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ function App() {
           <Route path='/register' element={<Register />} />
         </Routes>        
       </div>
+      <Footer />
     </Router>
     <ToastContainer />
     </>
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,15 +1,20 @@
 import { Link } from 'react-router-dom';
+import { FaYoutube, FaTwitter, FaReddit } from 'react-icons/fa';
 
 function Footer() {
   return (
-    <footer>
-      <div className="footer-content">
-        <p>&copy; {new Date().getFullYear()}All rights reserved.</p>
-        <nav>
-          <Link to="/privacy-policy">About</Link>
-          <Link to="/terms-of-service">FAQ</Link>
-          <Link to="/contact">Contact Us</Link>
-        </nav>
+    <footer className="footer">
+      <div className="footer-links">
+        <Link to="/about">ABOUT</Link>
+        <Link to="/faq">FAQ</Link>
+        <Link to="/contact">CONTACT US</Link>
+      </div>
+      
+      <div className="footer-icons">
+        <p>&copy; {new Date().getFullYear()} All rights reserved</p>
+        <a href="https://www.reddit.com" target="_blank" rel="noopener noreferrer"><FaReddit /></a>
+        <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
+        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
       </div>
     </footer>
   );
