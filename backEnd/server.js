e27bbac03812e66e608f8d2f5112a8ca2e93cff6
Carolina <csa71@gcloud.ua.es>
2025-05-20 13:53:51 +0200

    .

--- backEnd/server.js
+++ backEnd/server.js
@@ -14,30 +14,29 @@ const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
-// para poder subir archivos
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-
-
-app.listen(port, () => console.log(`server is running on port: ${port}`));
-app.use(errorHandler);
 app.use('/api/assets', require('./routes/assetsRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/categories', require('./routes/categoryRoutes'));
 app.use('/api/comments', require('./routes/commentRoutes'));
 app.use('/api/tags', require('./routes/tagRoutes'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
-//preparando para deployment
 if(process.env.NODE_ENV === 'production'){
    app.use(express.static(path.join(__dirname, '../frontend/build')));
-   app.get('*', (req, res) => res.sendFile(path.resolve(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))));
+   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')));
 }else{
     app.get('/', (req, res) => res.send('Please set to production'));
 }
+
+app.use(errorHandler);
+app.listen(port, () => console.log(`server is running on port: ${port}`));
+
+
+//preparando para deployment
+
 
 
 //usar npm run server para correr el servidorapp.use('/api/users', require('./routes/userRoutes'));
